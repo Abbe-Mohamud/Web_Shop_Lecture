49bcb6f64520e2863a775a8df6eff65f5822fc8c
Abbe-Mohamud <mohamud99@gmail.com>
2021-09-18 20:14:13 +0200

    WS 1

---
+++ src/main/java/se/lexicon/web_shop_lecture/WebShopLectureApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.web_shop_lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShopLectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebShopLectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/lexicon/web_shop_lecture/WebShopLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.web_shop_lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShopLectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
